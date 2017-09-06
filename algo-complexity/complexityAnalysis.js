072ddbae29f4550c0658c5aaba26f8732b78a62b
AlaaAllouzi <alaaallouzi91@gmail.com>
2017-09-06 12:20:14 +0300

    hahaha

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -3,4 +3,9 @@
 // Write your explanation below as a comment
 /*
 
+  1.constant :O(1) simple code  not complex you can wrote internal just var and for loop search in same parameter 
+  2.logarithmic : O(log) contain if statment (edgecases )  and some of recusion 
+  3.linear :O(n) contain one for loop  and evaluate simple function
+  4.quadraticL: O(n2) contain Nested for loop and solved more complex than linear
+  5.exponential : contain more and more edge cases if statement  and for for loop and recarion
 */
